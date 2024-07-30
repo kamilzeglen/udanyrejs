5a373274100ce4606d1620f186914e6fc8a172bf
Kamil Żegleń <kamyrdol32@gmail.com>
2024-07-31 00:10:11 +0200

    First component + fixes init

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -2,9 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'react-app',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
---
+++ web/src/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+
+  return (
+
+
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="flex items-center justify-between items-center px-6 py-4 text-white">Tutaj kiedys coś bedzie</div>
+    </nav>
+
+  )
+}
