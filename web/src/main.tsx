776eab4267e7e983cad30d8cc43504e6d7dbd056
WoX3R <wojciech.sidor2000@gmail.com>
2024-08-02 19:13:55 +0200

    test
    test

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './scss/index.scss'
 import Navbar from "./components/navbar.tsx";
+import ProductCard from './components/productCard.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
+  <ProductCard/>
+
+
   </React.StrictMode>,
 )
