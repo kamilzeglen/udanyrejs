85c971c5ffc4972ea807fa915f666ee86fe3d04f
Kamil Żegleń <kamyrdol32@gmail.com>
2024-12-30 12:18:01 +0100

    UdanyRejs 0.8.5

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,20 +1,55 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import * as bodyParser from 'body-parser';
+import {json, urlencoded} from "express";
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {readFileSync} from 'fs';
+import {NestApplicationOptions} from "@nestjs/common";
+
+const parsedConfig = require('dotenv').config();
+if (!parsedConfig.parsed || parsedConfig.error) {
+  throw Error('DOTENV did not return proper config');
+}
+const config = parsedConfig.parsed;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: process.env.WEB_URL,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      credentials: true,
-      allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-  console.log('Allowing origin: ', process.env.WEB_URL);
-
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
-  await app.listen(process.env.APP_PORT || 3000);
+  const {APP_PORT, HTTPS_ENABLED, HTTPS_CERTS_DIR, DOMAINS_WHITELIST, WEB_URL} = config;
+  const appOptions: NestApplicationOptions = {};
+  if (HTTPS_ENABLED === 'ENABLED' && HTTPS_CERTS_DIR) {
+    appOptions.httpsOptions = {
+      key: readFileSync(`${HTTPS_CERTS_DIR}/privkey.pem`),
+      cert: readFileSync(`${HTTPS_CERTS_DIR}/cert.pem`),
+    };
+  }
+  const whitelist = DOMAINS_WHITELIST?.split(',') || [];
+  if (!whitelist?.length) {
+    console.error('\n\n \t !!! Whitelist is empty or undefined !!! \n\n');
+  }
+  let origin: any = (origin: string, callback: (error: Error, isOriginAllowed: boolean) => {}) => {
+    if (!origin) {
+      callback(null, true);
+      return;
+    }
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+      return;
+    }
+    callback({ message: `NOT_ALLOWED`, name: 'CORS', stack: 'CORS' }, false);
+    return;
+  };
+  console.log('Allowing Web URL: ', WEB_URL)
+  console.log('Allowing domains: ', DOMAINS_WHITELIST)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    ...appOptions,
+  });
+  app.enableCors({origin, credentials: true});
+  app.use(json({limit: '50mb'}));
+  app.use(
+    urlencoded({
+      limit: '50mb',
+      extended: true,
+      parameterLimit: 5000,
+    }),
+  );
+  const server = await app.listen(APP_PORT || 3000);
 }
 bootstrap();
