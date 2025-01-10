ad6ffc1433f78d254e94d93fb419438104822726
Kamil Żegleń <kamyrdol32@gmail.com>
2025-01-10 13:15:25 +0100

    UdanyRejs 0.8.6

--- api/src/migrations/1736103439391-SeedAddCompaniesImages.ts
+++ api/src/migrations/1736103439391-SeedAddCompaniesImages.ts
@@ -1,52 +1,54 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {MigrationInterface, QueryRunner} from "typeorm";
+import * as path from 'path';
 
 export class SeedAddCompaniesImages1736103439391 implements MigrationInterface {
     name = 'SeedAddCompaniesImages1736103439391'
 
+  public imagesPath = process.env.COMPANIES_IMAGES_PATH;
+  public companyImages = [
+    {
+      companyId: '9532e2a2-1ad2-452a-be02-f0a849a6f363',
+      imageName: '9532e2a2-1ad2-452a-be02-f0a849a6f363.png',
+      originalName: 'MSC.png',
+      path: path.join(this.imagesPath, '9532e2a2-1ad2-452a-be02-f0a849a6f363.png'),
+    },
+    {
+      companyId: '531a42ab-9cfb-4217-8532-ea6d5d08d123',
+      imageName: '531a42ab-9cfb-4217-8532-ea6d5d08d123.png',
+      originalName: 'ROYAL_CARIBBEAN.png',
+      path: path.join(this.imagesPath, '531a42ab-9cfb-4217-8532-ea6d5d08d123.png'),
+    },
+    {
+      companyId: '521a42ab-8cfb-4977-8532-ea6d5c08d953',
+      imageName: '521a42ab-8cfb-4977-8532-ea6d5c08d953.png',
+      originalName: 'COSTA.png',
+      path: path.join(this.imagesPath, '521a42ab-8cfb-4977-8532-ea6d5c08d953.png'),
+    },
+    {
+      companyId: '521a42ab-8cfb-4357-8532-ea6d5c08d123',
+      imageName: '521a42ab-8cfb-4357-8532-ea6d5c08d123.png',
+      originalName: 'NCL.png',
+      path: path.join(this.imagesPath, '521a42ab-8cfb-4357-8532-ea6d5c08d123.png'),
+    },
+    {
+      companyId: '521a19ab-8cfb-4977-8532-ea6d5c08d953',
+      imageName: '521a19ab-8cfb-4977-8532-ea6d5c08d953.png',
+      originalName: 'AIDA.png',
+      path: path.join(this.imagesPath, '521a19ab-8cfb-4977-8532-ea6d5c08d953.png'),
+    },
+    {
+      companyId: '123a45ab-7cfb-4217-8532-ea6d5d08d123',
+      imageName: '123a45ab-7cfb-4217-8532-ea6d5d08d123.png',
+      originalName: 'TUI.png',
+      path: path.join(this.imagesPath, '123a45ab-7cfb-4217-8532-ea6d5d08d123.png'),
+    },
+  ]
+
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Dane do wprowadzenia (Array z danymi zdjęć oraz powiązanych firm)
-    const companyImages = [
-      {
-        companyId: '9532e2a2-1ad2-452a-be02-f0a849a6f363',
-        imageName: '9532e2a2-1ad2-452a-be02-f0a849a6f363.png',
-        originalName: 'MSC.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\9532e2a2-1ad2-452a-be02-f0a849a6f363.png',
-      },
-      {
-        companyId: '531a42ab-9cfb-4217-8532-ea6d5d08d123',
-        imageName: '531a42ab-9cfb-4217-8532-ea6d5d08d123.png',
-        originalName: 'ROYAL_CARIBBEAN.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\531a42ab-9cfb-4217-8532-ea6d5d08d123.png',
-      },
-      {
-        companyId: '521a42ab-8cfb-4977-8532-ea6d5c08d953',
-        imageName: '521a42ab-8cfb-4977-8532-ea6d5c08d953.png',
-        originalName: 'COSTA.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\521a42ab-8cfb-4977-8532-ea6d5c08d953.png',
-      },
-      {
-        companyId: '521a42ab-8cfb-4357-8532-ea6d5c08d123',
-        imageName: '521a42ab-8cfb-4357-8532-ea6d5c08d123.png',
-        originalName: 'NCL.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\521a42ab-8cfb-4357-8532-ea6d5c08d123.png',
-      },
-      {
-        companyId: '521a19ab-8cfb-4977-8532-ea6d5c08d953',
-        imageName: '521a19ab-8cfb-4977-8532-ea6d5c08d953.png',
-        originalName: 'AIDA.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\521a19ab-8cfb-4977-8532-ea6d5c08d953.png',
-      },
-      {
-        companyId: '123a45ab-7cfb-4217-8532-ea6d5d08d123',
-        imageName: '123a45ab-7cfb-4217-8532-ea6d5d08d123.png',
-        originalName: 'TUI.png',
-        path: 'X:\\udanyrejs-files\\COMPANIES_IMAGES\\123a45ab-7cfb-4217-8532-ea6d5d08d123.png',
-      },
-    ];
 
     // Iterowanie przez dane i tworzenie rekordów
-    for (const image of companyImages) {
+    for (const image of this.companyImages) {
       // Wstaw zdjęcie do tabeli ImageFile
       await queryRunner.query(
         `
@@ -78,27 +80,26 @@ export class SeedAddCompaniesImages1736103439391 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // Usunięcie zdjęć powiązanych z firmami (jeśli konieczne)
-    const paths = [
-      '/images/companies/company1.jpg',
-      '/images/companies/company2.jpg',
-      // Dodaj więcej ścieżek według potrzeb
-    ];
 
-    // Usuwanie rekordów z tabeli ImageFile
-    await queryRunner.query(
-      `
-      DELETE FROM "image_file"
-      WHERE "path" = ANY($1)
-    `,
-      [paths],
-    );
+    for (const image of this.companyImages) {
+      // Najpierw usuń powiązania w tabeli "company"
+      await queryRunner.query(
+        `
+        UPDATE "company"
+        SET "imageFileId" = NULL
+        WHERE "id" = $1
+      `,
+        [image.companyId],
+      );
 
-    // Usunięcie powiązania z firmami
-    await queryRunner.query(`
-      UPDATE "company"
-      SET "imageFileId" = NULL
-      WHERE "imageFileId" IS NOT NULL
-    `);
+      // Następnie usuń rekord z tabeli "image_file"
+      await queryRunner.query(
+        `
+        DELETE FROM "image_file"
+        WHERE "path" = $1
+      `,
+        [image.path],
+      );
+    }
   }
 }
