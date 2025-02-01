d74243744d024690f0d0b47eb66ed2dd68e8a743
Kamil Żegleń <kamyrdol32@gmail.com>
2025-02-01 14:35:03 +0100

    UdanyRejs 9.2

--- web/src/environments/environment.dev.ts
+++ web/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   PRODUCTION: true,
-  WEB_URL: 'https.udanyrejs.pl',
+  WEB_URL: 'https://dev.udanyrejs.pl',
   API_URL: 'https://api-dev.udanyrejs.pl',
   SCRAPPER_URL: 'https://scrapper-dev.udanyrejs.pl',
 };
--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   PRODUCTION: true,
-  WEB_URL: 'https.udanyrejs.pl',
+  WEB_URL: 'https://udanyrejs.pl',
   API_URL: 'https://api.udanyrejs.pl',
   SCRAPPER_URL: 'https://scrapper.udanyrejs.pl',
 };
