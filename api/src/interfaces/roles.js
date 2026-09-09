b48640b14eb8e620cb00bd7ac6ac9b726870b03f
Kamil Żegleń <kamyrdol32@gmail.com>
2026-09-10 00:51:10 +0200

    Remove compiled JS files accidentally committed to api/src
    These were stray build output sitting next to the TypeScript sources.
    Ignoring src/**/*.js going forward so a stray tsc run can't reintroduce them.

--- api/src/interfaces/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["USER"] = "USER";
-    Roles["ADMIN"] = "ADMIN";
-})(Roles || (exports.Roles = Roles = {}));
--- api/src/modules/offer/dto/create-offer.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateOfferDto = void 0;
-var CreateOfferDto = /** @class */ (function () {
-    function CreateOfferDto() {
-    }
-    return CreateOfferDto;
-}());
-exports.CreateOfferDto = CreateOfferDto;
