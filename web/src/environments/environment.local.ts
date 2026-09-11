c0e0fdd273f85e77572eba75a35cffca55d7ec3b
Kamil Żegleń <kamyrdol32@gmail.com>
2026-09-11 02:33:24 +0200

    Point the default dev environment at localhost instead of prod
    The dev build configuration should talk to a locally running API by
    default, not the deployed one.

--- web/src/environments/environment.local.ts
+++ web/src/environments/environment.local.ts
@@ -1,5 +1,5 @@
 export const environment = {
   PRODUCTION: true,
-  WEB_URL: 'https://udanyrejs.kamilzeglen.pl',
-  API_URL: 'https://udanyrejs-api.kamilzeglen.pl',
+  WEB_URL: 'http://localhost:4200',
+  API_URL: 'http://localhost:5006',
 };
